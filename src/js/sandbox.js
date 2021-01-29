1dafdf8b7e07a203434b2dc353ebba5a6d21ceac
michaeloconn98 <michael.oconn@hotmail.co.uk>
2021-01-29 13:22:15 +0000

    Basic Javascript

--- src/js/sandbox.js
+++ src/js/sandbox.js
@@ -1,1 +1,35 @@
-alert('hello world');
+// const para = document.querySelector('div.error');
+
+// console.log(para);
+
+// const paras = document.querySelectorAll('p')
+// const errors = document.querySelectorAll('.error')
+
+/*paras.forEach(para => {
+    console.log(para);
+})*/
+
+// console.log(errors)
+
+const para = document.querySelector('p');
+
+// console.log(para.innerText)
+// para.innerText = 'ninjas are awesome!';
+
+const paras = document.querySelectorAll('p');
+
+/* paras.forEach(para => {
+    console.log(para.innerText);
+    para.innerText += 'new test';
+})*/
+
+const content = document.querySelector('.content');
+
+// console.log(content.innerHTML);
+// content.innerHTML = '<h2>This is a new h2</h2>'
+
+/* const people = ['mario', 'luigi', 'yoshi'];
+
+people.forEach(person => {
+    content.innerHTML += '<p>${person}</p>';
+})*/
